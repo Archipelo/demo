06f9d956dd14595ea6a99388e11130b5443d43a0
gr0 <r.kuc@solr.pl>
2023-11-13 16:53:20 +0100

    Leak secret

---
+++ jsapps/apps/test/build.js
@@ -0,0 +1,6 @@
+const exportStringOptions = () => {
+    return JSON.stringify({
+        SENTRY_DSN: 'https://7e1b83de28dvxe27dv37d2d32@o1112823.ingest.sentry.io/614293232165',
+        MIX_PANEL_TOKEN: 'diub238yd8236d3v826327d27fd23'
+    });
+};
